1dd73a4fab70a8e65e126cffdec8611f042bffb5
abdelmawlaSouat <abdelmawla.souat@gmail.com>
2020-11-25 23:58:18 +0100

    Setup Project (dependencies, images)

---
+++ src/components/app/App.jsx
@@ -0,0 +1,20 @@
+/*
+ * App.jsx
+ *
+ * Author: Abdelmawla Souat (abdelmawla.souat@gmail.com)
+ * Created on 2020/11/25 23:55:05
+ *
+ * Copyright (c) 2020 Shuriken
+ */
+
+import './App.css'
+
+function App() {
+	return (
+		<div className="App">
+			<h1>Hello world</h1>
+		</div>
+	)
+}
+
+export default App
---
+++ src/index.jsx
@@ -0,0 +1,20 @@
+/*
+ * index.jsx
+ *
+ * Author: Abdelmawla Souat (abdelmawla.souat@gmail.com)
+ * Created on 2020/11/25 23:55:13
+ *
+ * Copyright (c) 2020 Shuriken
+ */
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './components/app/App'
+
+ReactDOM.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById('root')
+)
