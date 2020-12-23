55412def5439d1aa1712775c5aece2c2069deff6
Abdelmawla Souat <abdelmawla.souat@gmail.com>
2020-12-23 17:22:28 +0100

    Add Header Background

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,20 +4,43 @@
  * @author Abdelmawla Souat <abdelmawla.souat@gmail.com>
  *
  * Created at     : 2020-11-25 23:55:05
- * Last modified  : 2020-12-23 15:04:48
+ * Last modified  : 2020-12-23 17:19:19
  */
 
-import { Container, CssBaseline, Grid, ThemeProvider } from '@material-ui/core'
+import {
+	Box,
+	Container,
+	CssBaseline,
+	Grid,
+	ThemeProvider,
+} from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+
 import theme from '../themes/theme'
 import jobs from '../data/jobs.json'
 import JobCard from './JobCard'
 
+const useStyles = makeStyles(() => ({
+	header: {
+		background: "url('./images/bg-header-desktop.svg')",
+		height: '15vh',
+		backgroundRepeat: 'no-repeat',
+		[theme.breakpoints.only('xs')]: {
+			background: "url('./images/bg-header-mobile.svg')",
+		},
+	},
+}))
+
 function App() {
+	const classes = useStyles()
 	return (
 		<ThemeProvider theme={theme}>
 			<CssBaseline>
-				<Container className="app">
-					<Grid container className="job-listings" justify="center" spacing={5}>
+				<Box mb={2} style={{ background: 'hsl(180, 29%, 50%)' }}>
+					<Box className={classes.header} />
+				</Box>
+				<Container className="App">
+					<Grid container className="Job-listings" justify="center" spacing={5}>
 						{jobs.map((job) => (
 							<Grid item key={job.id} xs={10}>
 								<JobCard job={job} />
